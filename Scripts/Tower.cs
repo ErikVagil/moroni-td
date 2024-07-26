3570b45c804d89dffc532ba05f4593fe4b3a7b94
Erik Vagil <erikvagil2734@gmail.com>
2024-07-26 10:24:33 -0500

    Basic Tower placement

---
+++ Scripts/Tower.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Tower : Area2D
+{
+	private bool isInBuildMode = true;
+	private bool isColliding = false;
+	private readonly HashSet<Area2D> collidingAreas = new();
+	private Color highlightGood = new(1f, 1f, 1f, 0.75f);
+	private Color highlightBad = new(1f, 0.5f, 0.5f, 0.75f);
+	private Color defaultMod = new(1f, 1f, 1f, 1f);
+
+    public override void _Process(double delta) {
+		if (isInBuildMode) {
+			// Highlight if in build mode
+			if (isColliding) Modulate = highlightBad;
+			else Modulate = highlightGood;
+
+			// Follow the cursor
+			Position = GetGlobalMousePosition();
+
+			if (Input.IsActionJustPressed("build_accept")) {
+				isInBuildMode = false;
+			}
+		} else {
+			Modulate = defaultMod;
+		}
+	}
+
+	private void OnAreaEntered(Area2D area) {
+		collidingAreas.Add(area);
+		isColliding = true;
+		GD.Print($"Collided with {area}");
+	}
+
+	private void OnAreaExited(Area2D area) {
+		collidingAreas.Remove(area);
+		if (collidingAreas.Count == 0) {
+			isColliding = false;
+		}
+		GD.Print($"Stopped colliding with {area}");
+	}
+}
